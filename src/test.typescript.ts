d9c8daeba90426cfe301999715d1c6b009c9cc98
Raul Reci <reciraul@gmail.com>
2023-05-08 20:51:09 +0300

    change TS config

---
+++ src/test.typescript.ts
@@ -0,0 +1,16 @@
+function getConf() {
+  return {
+    name: "my conf",
+    x: 123,
+    ready: true
+  };
+}
+
+const conf = getConf();
+// console.info(conf.nume);
+
+//conf.ready = "not ready";
+
+function a(b) {
+  return b * 2;
+}
